5ff2ebe6a4315c4b65c0cd837fc863b304767793
Jamshidbek <jamshidbek1805@gmail.com>
2022-07-14 10:22:47 +0500

    added delete,attandance,getgroup,list groups methods in schedule servce

--- schedule-service/storage/repo/user.go
+++ schedule-service/storage/repo/user.go
@@ -8,4 +8,8 @@ import (
 type UserStorageI interface {
 	CreateGroup(*pb.CreateGroupReq) (*pb.CreateGroupRes, error)
 	GetAllGroup(*pb.Empty) (*pb.GetAllGroupRes, error)
+	DeleteGroup(*pb.DeleteGroupReq)(*pb.Empty,error)
+	Attandence([]*pb.AttandenceRequa)(*pb.Empty,error)
+	GetGroup(*pb.GetGroupReq)(*pb.GetGroupRes,error)
+	ListGroups(*pb.ListGroupsReq)(*pb.ListGroupsRes,error)
 }
